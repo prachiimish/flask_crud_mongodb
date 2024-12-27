369d8008edf2b62870bb520910af5340420effd8
prachiimish <84406544+prachiimish@users.noreply.github.com>
2024-12-27 15:36:18 +0530

    Updated run.py

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
 
-# Create an instance of the Flask app
 app = create_app()
 
-# Run the app on localhost with debugging enabled
 if __name__ == "__main__":
     app.run(debug=True)
