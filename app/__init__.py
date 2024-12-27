30263f07978f24d8dfa7246bfb9b7e909fdf8c15
prachiimish <84406544+prachiimish@users.noreply.github.com>
2024-12-27 15:38:20 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
-# Initialize PyMongo
 mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config['MONGO_URI'] = 'mongodb://localhost:27017/usersdb'  # MongoDB URI
-    mongo.init_app(app)  # Initialize MongoDB with Flask app
+    mongo.init_app(app) 
 
-    # Register Blueprints
     from .routes import user_routes
-    app.register_blueprint(user_routes)  # Register the user_routes blueprint
+    app.register_blueprint(user_routes)  # Register user_routes blueprint
 
     return app
